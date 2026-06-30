220b6a7faac21ac5d70013d95e341eae648ea71f
nahuelmed21 <nahuelmedinautn@gmail.com>
2026-06-30 09:29:01 -0300

    feat: agrego interacción a galería de álbumes

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("cargar JavaScript.");
+
+function mostrarAlbum(evento) {
+    const album = evento.target.dataset.album;
+
+    document.getElementById("infoAlbum").textContent =
+        `Seleccionaste el álbum: ${album}`;
+}
+
+const imagenes = document.querySelectorAll(".album img");
+
+imagenes.forEach((imagen) => {
+    imagen.addEventListener("click", mostrarAlbum);
+});
