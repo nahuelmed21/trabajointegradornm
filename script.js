9650c4b32d600ecb64545856162540e54814cb25
nahuelmed21 <nahuelmedinautn@gmail.com>
2026-06-30 10:18:23 -0300

    feat: implemento validación del formulario

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 console.log("cargar JavaScript.");
 
 function mostrarAlbum(evento) {
-    const album = evento.target.dataset.album;
+  const album = evento.target.dataset.album;
 
-    document.getElementById("infoAlbum").textContent =
-        `Seleccionaste el álbum: ${album}`;
+  document.getElementById("infoAlbum").textContent =
+    `Seleccionaste el álbum: ${album}`;
 }
 
 const imagenes = document.querySelectorAll(".album img");
 
 imagenes.forEach((imagen) => {
-    imagen.addEventListener("click", mostrarAlbum);
+  imagen.addEventListener("click", mostrarAlbum);
 });
+
+const formulario = document.getElementById("formContacto");
+
+formulario.addEventListener("submit", validarFormulario);
+
+function validarFormulario(evento) {
+  evento.preventDefault();
+
+  const mensajeFormulario = document.getElementById("mensajeFormulario");
+
+  try {
+    const nombre = document.getElementById("nombre").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const mensaje = document.getElementById("mensaje").value.trim();
+
+    if (nombre === "" || email === "" || mensaje === "") {
+      throw new Error("Todos los campos son obligatorios.");
+    }
+    mensajeFormulario.textContent = "Formulario enviado correctamente.";
+  } catch (error) {
+    mensajeFormulario.textContent = error.message;
+  }
+}
